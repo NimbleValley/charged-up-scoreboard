cd2ee58d58ff5d31d20ce9d70aed9320b1454c71
Mason McManus <97319135+NimbleValley@users.noreply.github.com>
2023-03-14 18:54:15 -0500

    Added BzzBzzBzz hahahahaha

--- counter.js
+++ counter.js
@@ -1,10 +1,21 @@
 var score = 0;
 
 const title = document.getElementById("title");
-
 const lights = document.getElementsByClassName("lights");
+const root = document.querySelector(':root');
 
 var ok = false;
+
+const blueBackground = "rgb(0, 30, 179)";
+const blueFancy = "rgb(27, 88, 255)";
+const redBackground = "rgb(179, 0, 0)";
+const redFancy = "rgb(255, 27, 27)";
+const orangeBackground = "rgb(137, 71, 0)";
+const orangeFancy = "rgb(196, 97, 10)";
+
+var buzzer = new Audio('sounds/buzzer.wav');
+var bzzbzzbzz = new Audio('sounds/bzzbzzbzz.wav');
+var click = new Audio('sounds/click.mp3');
 
 document.addEventListener('keypress', function (event) {
     if (event.key == "q") {
@@ -25,12 +36,42 @@ document.addEventListener('keypress', function (event) {
     if (event.key == "5") {
         score += 5;
     }
+    if (event.key == "l") {
+        score += 5;
+    }
     if (event.key == "-") {
         score--;
     }
+    if (event.key == "?") {
+        score=3197;
+    }
+
+    if(event.key == "n") {
+        bzzbzzbzz.play();
+        return;
+    }
+
+    if(event.key == "m") {
+        buzzer.play();
+        return;
+    }
+
     if (score < 0) {
         score = 0;
     }
+
+    if (event.key == "b") {
+        switchBlue();
+    }
+    if (event.key == "r") {
+        switchRed();
+    }
+    if (event.key == "o") {
+        switchOrange();
+    }
+
+    click.play();
+
     updateScore();
 });
 
@@ -46,4 +87,19 @@ function updateScore() {
         }
     }
     ok = !ok;
+}
+
+function switchBlue() {
+    root.style.setProperty("--background-color", blueBackground);
+    root.style.setProperty("--fancy-color", blueFancy);
+}
+
+function switchRed() {
+    root.style.setProperty("--background-color", redBackground);
+    root.style.setProperty("--fancy-color", redFancy);
+}
+
+function switchOrange() {
+    root.style.setProperty("--background-color", orangeBackground);
+    root.style.setProperty("--fancy-color", orangeFancy);
 }
